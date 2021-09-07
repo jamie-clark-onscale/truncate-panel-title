04a16a55407699f7e73095807ee958a7b59d8754
jclark <79856316+jamie-clark-onscale@users.noreply.github.com>
2021-09-07 09:21:18 +0100

    panel title example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'get-truncated-string';
+export class AppComponent implements OnInit {
+  private selectedItems: string[] = ['an item', 'another item', 'some other item', 'name', 'selected', 'lorum'];
+  public panelTitle: string;
+  public panelTitleSuffix: number;
+
+  ngOnInit(): void {
+    this.panelTitle = this.selectedItems.join(', ');
+    this.panelTitleSuffix = this.selectedItems.length;
+  }
 }
